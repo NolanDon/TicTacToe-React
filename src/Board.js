3306eea05ad0200bc2160a44115398d24c798c95
Nolan <nolan.boxill@neofinancial.com>
2020-08-03 21:23:34 -0600

    adding reset game function

--- src/Board.js
+++ src/Board.js
@@ -52,14 +52,14 @@ function Board() {
   function calculateWinner(squares) {
 
     const winning = [
-      [0,1,2],
-      [3,4,5],                        
-      [6,7,8],
-      [0,3,6],
+      [1,2,3],
+      [4,5,6],                        
+      [7,8,9],
       [1,4,7],
       [2,5,8],
-      [0,4,8],
-      [2,4,6],
+      [3,6,9],
+      [1,5,9],
+      [3,5,7],
     ]
 
     for (let i = 0; i < winning.length; i++) {
@@ -84,20 +84,25 @@ function Board() {
     <div>
       <div className={classes.status}>{status}</div>
       <div className={classes.boardRow}>
-        {renderSquare(0)}
         {renderSquare(1)}
         {renderSquare(2)}
+        {renderSquare(3)}
       </div>
       <div className={classes.boardRow}>
-        {renderSquare(3)}
         {renderSquare(4)}
         {renderSquare(5)}
+        {renderSquare(6)}
       </div>
       <div className={classes.boardRow}>
-        {renderSquare(6)}
         {renderSquare(7)}
         {renderSquare(8)}
+        {renderSquare(9)}
       </div>
+      <button onClick={() => {
+        resetGame()
+      }}>
+        reset
+      </button>
     </div>
   );
 }
