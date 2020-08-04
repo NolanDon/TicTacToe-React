0e080d9ab390f2c83a2b15c7c8a6cc4f407d853c
Nolan <nolan.boxill@neofinancial.com>
2020-08-03 21:43:21 -0600

    final code commit

--- src/Board.js
+++ src/Board.js
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
-import { makeStyles } from '@material-ui/core';
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles({
   boardRow: {
-    display: 'flex',
-    justifyContent: 'center',
-    overflowWrap: 'wrap',
-        height: '50px'
+    display: "flex",
+    justifyContent: "center",
+    overflowWrap: "wrap",
+    height: "50px",
   },
-  button: { 
-    width: '50px',
-    height: '50px'
+  buttons: {
+    margin: '30px',
+    textAlign: 'center'
+  },
+  button: {
+    width: "50px",
+    height: "50px",
+  },
+  resetButton: {
+    textAlign: "center",
   },
   status: {
-    textAlign: 'center',
-    margin: '50px'
-  }
-})
+    textAlign: "center",
+    margin: "50px",
+  },
+});
 
 function Board() {
   const [boardSquares, setBoardSquares] = useState([Array(9).fill(null)]);
@@ -33,10 +40,13 @@ function Board() {
 
     setXIsNext(!xIsNext);
   };
-  
 
   function Square(props) {
-    return <button className={classes.button} onClick={props.onClick}>{props.value}</button>;
+    return (
+      <button className={classes.button} onClick={props.onClick}>
+        {props.value}
+      </button>
+    );
   }
 
   const renderSquare = (index) => {
@@ -46,42 +56,44 @@ function Board() {
   };
 
   function calculateWinner(squares) {
-
     const winning = [
-      [1,2,3],
-      [4,5,6],                        
-      [7,8,9],
-      [1,4,7],
-      [2,5,8],
-      [3,6,9],
-      [1,5,9],
-      [3,5,7],
-    ]
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [1, 5, 9],
+      [3, 5, 7],
+    ];
 
     for (let i = 0; i < winning.length; i++) {
-      const [a,b,c] = winning[i]
+      const [a, b, c] = winning[i];
 
-      if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      console.log(squares.length);
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[b] === squares[c]
+      ) {
         return squares[a];
       }
     }
-    return null
+    return null;
   }
 
   const resetGame = () => {
     setBoardSquares([Array(9).fill(null)]);
-  }
+  };
 
   let status;
-  const winner = calculateWinner(boardSquares)
-  status = winner ?
-  `Winner = ${winner}`
-  :
-  `Next Player: ${xIsNext ? 'X' : 'O'}`
-
+  const winner = calculateWinner(boardSquares);
+  status = winner
+    ? `Winner = ${winner}`
+    : `Next Player: ${xIsNext ? "X" : "O"}`;
 
   return (
-    <div>
+    <div className={classes.board}>
       <div className={classes.status}>{status}</div>
       <div className={classes.boardRow}>
         {renderSquare(1)}
@@ -98,12 +110,28 @@ function Board() {
         {renderSquare(8)}
         {renderSquare(9)}
       </div>
-      {calculateWinner(boardSquares) ? 
-        <button className={classes.resetButton} onClick={() => {
-          resetGame()
-        }}>
+    <div className={classes.buttons}>
+      {calculateWinner(boardSquares) ? (
+        <button
+          className={classes.resetButton}
+          onClick={() => {
+            resetGame();
+          }}
+        >
           New Match
-        </button> : null}
+        </button>
+      ) : (
+        <button
+          className={classes.resetButton}
+          onClick={() => {
+            resetGame();
+          }}
+        >
+          Reset Game
+        </button>
+        
+      )}
+      </div>
     </div>
   );
 }
